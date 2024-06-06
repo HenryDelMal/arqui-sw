e1375587c0f7a770e68c40b807f2ec292b60d1c8
Henry López del Pino <henry.lopez@mail.udp.cl>
2024-06-05 22:23:19 -0400

    hola

--- servicios/include/bus_functions.py
+++ servicios/include/bus_functions.py
@@ -8,6 +8,14 @@ print ('connecting to {} port {}'.format (*bus_address))
 sock.connect (bus_address)
 
 def generate_sinit(service_name):
-    length = len(service_name)
-    message = f"{length:05d}{'sinit'[:5]}{service_name}".encode()
+    length = len('sinit'+service_name)
+    message = f"{length:05d}{'sinit'}{service_name}".encode()
     return message
+
+def extract_string_bus(input_string):
+    input_string = input_string.decode()  # Convert byte string to regular string
+    length = int(input_string[:5])
+    service_name = input_string[5:10]  # Extract the next 5 characters as the service name
+    status = input_string[10]  # Extract the status character
+    command = input_string[11:length+10]  # Skip the status and service name, and extract the rest of the string
+    return service_name, status, command
--- servicios/servicio1.py
+++ servicios/servicio1.py
@@ -1,23 +1,31 @@
 from include.bus_functions import *
 
+service_name = "serv1"
+
 try:
-    # Send data
-    message = b'00010sinitservi'
+
+    # Send SINIT to the bus
+    message = generate_sinit(service_name)
     print ('sending {!r}'.format (message))
     sock.sendall (message)
     sinit = 1
     while True:
     # Look for the response
         amount_received = 0
-        amount_expected = 10
+        amount_expected = int(sock.recv(5))
         while amount_received < amount_expected:
-            data = sock.recv (16)
+            data = sock.recv (amount_expected - amount_received)
             amount_received += len(data)
             print ('received {!r}'.format (data))
-            if data == b'00010sinitok':
-                print("sinit ok")
-                break
+
+            if data == b'sinitOK'+service_name.encode():
+                 print("sinit ok")
+                 break
         break
+
+
+
+
 finally:
     print ('closing socket')
     sock.close()
