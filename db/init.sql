10eefcc616acfdd1fd1bea56dd6e873e686f4a49
Henry López del Pino <henry.lopez@mail.udp.cl>
2024-06-06 22:33:26 -0400

    agrego bdd

---
+++ db/init.sql
@@ -0,0 +1,12 @@
+CREATE TABLE users (
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    type VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE buses (
+    patente VARCHAR(6) NOT NULL,
+    modelo VARCHAR(50) NOT NULL,
+    estado VARCHAR(50) NOT NULL
+);
+
