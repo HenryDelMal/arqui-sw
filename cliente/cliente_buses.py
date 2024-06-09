61de9cc98edc4f3e89d4c78b067a8a5b4833039d
cristobal-nan <cristobal.nan@gmail.com>
2024-06-08 21:53:48 -0400

    servicio buses

---
+++ cliente/cliente_buses.py
@@ -0,0 +1,35 @@
+from include.bus_functions import *
+
+service_name = "BUSES"
+
+bus = input("Ingrese bus: ")
+
+try:
+    #print ("Send command")
+    message = generate_string(service_name, '{}'.format(bus))
+    #message = generate_string(service_name, 'usuario1,1234')
+    #print('sending {!r}'.format (message))
+    sock.sendall (message)
+    while True:
+    # Look for the response
+        amount_received = 0
+        amount_expected = int(sock.recv(5))
+
+        while amount_received < amount_expected:
+            data = sock.recv (amount_expected - amount_received)
+            amount_received += len(data)
+            #print ('received {!r}'.format (data))
+            service_name, status, answer = extract_string_bus(data)
+            #print("Status:", status)
+            #print("Service Name:", service_name)
+            #print("Answer:", answer)
+            if answer == "ERROR" or status == "NK":
+                print("Error al ingresar.")
+            else:
+                print('Estado {}.'.format(answer))
+            break
+        break
+
+finally:
+    #print ('closing socket')
+    sock.close()
