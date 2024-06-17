9e85f83ee06d5000e475f270b7cd079db3d8a58b
Pablo Ahumada <pablo.ahumada1@mail.udp.cl>
2024-06-17 03:25:18 -0400

    registro_buses added

--- cliente/admin.py
+++ cliente/admin.py
@@ -67,6 +67,29 @@ def registro(service_name):
                 print('Se ha ingresado correctamente')
                 return username, answer
         break
+def registro_bus(service_name):
+    print(service_name)
+                
+    patente = input("Ingrese patente: ")
+    modelo = input("Ingrese modelo: ")
+    estado = input("Ingrese estado: ")
+    message = generate_string(service_name, 'INS,{},{},{}'.format(patente, modelo, estado))
+    sock.sendall(message)
+    while True:
+        amount_received = 0
+        amount_expected = int(sock.recv(5))
+
+        while amount_received < amount_expected:
+            data = sock.recv(amount_expected - amount_received)
+            amount_received += len(data)
+            service_name, status, answer = extract_string_bus(data)
+            if answer == "ERROR" or status == "NK":
+                print("Error al ingresar.")
+                return None, None
+            else:
+                print('Se ha ingresado correctamente')
+                return patente, answer
+        break
 
 def main():
     logged_in = False
@@ -90,18 +113,22 @@ def main():
             print("Seleccione una opción:")
             
         print("1. Registrar usuario")
-        print("2. Salir")
+        print("2. Registrar bus")
+        print("3. Salir")
         
         option = input("Ingrese opción: ")
 
-        if option == '1':
-            registro("REGIS")
-        elif option == '2':
-            print("Saliendo del programa.")
-            sock.close()
-            break
-        else:
-            print("Opción no válida. Por favor, ingrese 1 o 2.")
+        match option:
+            case '1':
+                registro("REGIS")
+            case '2':
+                registro_bus("BUSES")
+            case '3':
+                print("Saliendo del programa.")
+                sock.close()
+                break
+            case _:
+                print("Opción no válida. Por favor, ingrese 1 o 2.")
 
 if __name__ == "__main__":
     main()
